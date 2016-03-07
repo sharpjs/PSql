9fed296409a03fafb4fb85ae3352b82dd1209a70
Jeff Sharp <jeff.sharp@git>
2016-03-07 16:08:33 -0600

    Add Invoke-SqlModulesParallel

--- PSql.Make.ps1
+++ PSql.Make.ps1
@@ -140,95 +140,160 @@ function Merge-SqlModules {
     }
 }
 
-function Complete-SqlModule($Module, $ModuleSet) {
-    $Queue        = $ModuleSet.Queue
-    $Subjects     = $ModuleSet.Subjects
-    $QueueChanged = $false
+function Invoke-SqlModulesParallel {
+    param ($ModuleSet)
 
-    # Update each subject provided by this module
-    foreach ($Name in $Module.Provides) {
-        $Subject = $Subjects[$Name]
+    $OldInformationPreference = $InformationPreference
+    $InformationPreference = "Continue"
 
-        # Mark this module as done
-        $Subject.ProvidedBy.Remove($Module)
+    $Workers = foreach ($Id in 1..$env:NUMBER_OF_PROCESSORS) {
+        Write-Information "[Thread $Id]: Starting."
 
-        # Check if all subject's modules are done
-        if ($Subject.ProvidedBy) { continue }
+        # Create worker thread to run modules asynchronously
+        $p = [powershell]::Create()
+        $p.Runspace.SessionStateProxy.SetVariable('ModuleSet', $ModuleSet)
+        $p.AddScript($ThreadMain) | Out-Null
 
-        # Mark subject as done
-        $Subjects.Remove($Name)
+        # Pipe worker output streams to main output streams
+        foreach ($Kind in 'Debug', 'Verbose', 'Information', 'Warning' ,'Error') {
+            Register-ObjectEvent $p.Streams.$Kind DataAdded -SupportEvent `
+                -MessageData @{ Id = $Id; Kind = $Kind } `
+                -Action {
+                    $Id   = $Event.MessageData.Id
+                    $Kind = $Event.MessageData.Kind
+                    $Event.Sender.ReadAll() | % {
+                        if ($Kind -ne 'Error') {
+                            & "Write-$Kind" "[Thread $Id]: $_"
+                        } else {
+                            $Host.UI.WriteErrorLine("[Thread $Id]: $_")
+                        }
+                    }
+                } `
+            | Out-Null 
+        }
 
-        # Update dependents
-        foreach ($Dependent in $Subject.RequiredBy) {
+        # Start worker
+        $r = $p.BeginInvoke()
+        @{ Id = $Id; Shell = $p; Invocation = $r }
+    }
 
-            # Mark requirement as met
-            $Dependent.Requires.Remove($Name)
-
-            # Check if all of dependent's requrements are met
-            if ($Dependent.Requires) { continue }
-
-            # All requirements met; queue the dependent
-            $Queue.Enqueue($Dependent)
-            $QueueChanged = $true
+    # Wait for workers to finish
+    $Failed = $false
+    foreach ($Worker in $Workers) {
+        try {
+            $Worker.Shell.EndInvoke($Worker.Invocation)
+            Write-Information "[Thread $($Worker.Id)]: Ended."
+        }
+        catch {
+            $Failed = $true
+            $Host.UI.WriteErrorLine("[Thread $($Worker.Id)]: $($_.Exception.Message)")
+            $Host.UI.WriteErrorLine("[Thread $($Worker.Id)]: Terminated due to exception.")
         }
     }
 
-    if ($QueueChanged) {
-        [System.Threading.Monitor]::PulseAll($Queue)
+    if ($Failed) {
+        throw 'One or more threads ended with an error.'
     }
+
+    $InformationPreference = $OldInformationPreference 
 }
 
-function Invoke-SqlModules ($ModuleSet) {
-    $Queue    = $ModuleSet.Queue
-    $Subjects = $ModuleSet.Subjects
+$ThreadMain = {
+    function Invoke-SqlModules {
+        param ($ModuleSet)
 
-    $Module     = $null
-    $Connection = $null
+        $Queue      = $ModuleSet.Queue
+        $Subjects   = $ModuleSet.Subjects
+        $Module     = @{ Value = $null }
+        $Connection = $null
 
-    while ($true) {
-        # Advance to next module
-        $Module = Use-Lock $Queue {
-            # Mark prior module completed
-            if ($Module) {
-                Complete-SqlModule $Module $ModuleSet
-            }
+        while ($true) {
+            # Advance to next module
+            Use-Lock $Queue {
+                # Mark prior module completed
+                if ($Module["Value"]) {
+                    Complete-SqlModule $Module["Value"] $ModuleSet
+                }
 
-            # Dequeue next module
-            while ($true) {
-                if ($Queue.Count) {
-                    # Take next queued module
-                    $Queue.Dequeue() | Write-Output
-                    break
-                } elseif (!$Subjects.Count) {
-                    # No modules queued, none in progress; done
-                    break
-                } else {
-                    # Wait for other modules to finish
-                    [System.Threading.Monitor]::Wait($Queue)
+                # Dequeue next module
+                while ($true) {
+                    if ($Queue.Count) {
+                        # Take next queued module
+                        $Module["Value"] = $Queue.Dequeue()
+                        break
+                    } elseif (!$Subjects.Count) {
+                        # No modules queued, none in progress; done
+                        $Module["Value"] = $null
+                        break
+                    } else {
+                        # Wait for other modules to finish
+                        [System.Threading.Monitor]::Wait($Queue)
+                    }
                 }
             }
+
+            # Check if done
+            if (!$Module["Value"]) { return }
+
+            # Run module
+            Write-Host "Invoking $($Module["Value"].Name)" -ForegroundColor Yellow
         }
 
-        # Check if done
-        if (!$Module) { return }
-
-        # Run module
-        Write-Host "Invoking $($Module.Name)" -ForegroundColor Yellow
+        #Disconnect-Sql $Connection
     }
 
-    Disconnect-Sql $Connection
-}
+    function Complete-SqlModule($Module, $ModuleSet) {
+        $Queue        = $ModuleSet.Queue
+        $Subjects     = $ModuleSet.Subjects
+        $WorkChanged = $false
 
-function Use-Lock {
-    param([object] $Object, [scriptblock] $ScriptBlock)
-    $Locked = $false
-    try {
-        [System.Threading.Monitor]::Enter($Object, [ref] $Locked)
-        & $ScriptBlock
-    }
-    finally {
-        if ($Locked) {
-            [System.Threading.Monitor]::Exit($Object)
+        # Update each subject provided by this module
+        foreach ($Name in $Module.Provides) {
+            $Subject = $Subjects[$Name]
+
+            # Mark this module as done
+            $Subject.ProvidedBy.Remove($Module)
+
+            # Check if all subject's modules are done
+            if ($Subject.ProvidedBy) { continue }
+
+            # Mark subject as done
+            $Subjects.Remove($Name)
+            if (!$Subjects.Count) { $WorkChanged = $true }
+
+            # Update dependents
+            foreach ($Dependent in $Subject.RequiredBy) {
+
+                # Mark requirement as met
+                $Dependent.Requires.Remove($Name)
+
+                # Check if all of dependent's requrements are met
+                if ($Dependent.Requires) { continue }
+
+                # All requirements met; queue the dependent
+                $Queue.Enqueue($Dependent)
+                $WorkChanged = $true
+            }
+        }
+
+        if ($WorkChanged) {
+            [System.Threading.Monitor]::PulseAll($Queue)
         }
     }
+
+    function Use-Lock {
+        param([object] $Object, [scriptblock] $ScriptBlock)
+        $Locked = $false
+        try {
+            [System.Threading.Monitor]::Enter($Object, [ref] $Locked)
+            & $ScriptBlock
+        }
+        finally {
+            if ($Locked) {
+                [System.Threading.Monitor]::Exit($Object)
+            }
+        }
+    }
+
+    Invoke-SqlModules $ModuleSet
 }
