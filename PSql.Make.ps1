7c35248c2d8ff09cf4a25a9d39b7d1704427107d
Jeff Sharp <jeff.sharp@git>
2016-03-08 19:02:43 -0600

    Try A C#-based module runner to get around unidentified thread unsafety.

--- PSql.Make.ps1
+++ PSql.Make.ps1
@@ -25,12 +25,13 @@
 #>
 
 <#
-
---# MODULE
---# REQUIRES: Bcd
---# PROVIDES: Bldkf
-
+--# MODULE:   A
+--# PROVIDES: B C D
+--# REQUIRES: E F G H I
 #>
+
+Get-Content $PSScriptRoot\PSql.ModuleRunner.cs -Raw `
+    | % { Add-Type $_ -Language CSharp }
 
 $LinesRe     = [regex] '\r?\n'
 $SpacesRe    = [regex] '\s+'
@@ -38,131 +39,55 @@ $DirectiveRe = [regex] '(?x)
     ^ --\# \s+ (?<dir>MODULE|PROVIDES|REQUIRES): \s+ (?<args>.*) $
 '
 
-function New-SqlModule {
-    [CmdletBinding()]
-    [OutputType([PSCustomObject])]
-    param(
-        [Parameter(Position=1, Mandatory, ValueFromPipeline)]
-        [string] $Name
-    )
-    process {
-        $Module = [PSCustomObject]@{
-            Name       = $Name
-            Provides   = New-Object System.Collections.ArrayList
-            Requires   = New-Object System.Collections.ArrayList
-            Script     = New-Object System.Text.StringBuilder 4096
-        }
-        $List = if ($Name -eq 'init') {"Provides"} else {"Requires"}
-        $Module.$List.Add('init') | Out-Null
-        $Module
-    }
-}
-
-function Out-SqlModule($Module) {
-    if ($Module.Provides.Count -or
-        $Module.Requires.Count -or
-        $Module.Script.Length) { Write-Output $Module }
-}
-
 function Read-SqlModules {
     [CmdletBinding()]
-    [OutputType([object[]])]
+    [OutputType([PSql.ModuleRunner])]
     param (
         # The text to process.
-        [Parameter(ValueFromPipeline)]
-        [string] $Text
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string] $Text,
+
+        [Parameter(Position = 2, Mandatory)]
+        [PSql.ModuleRunner] $Runner
     )
     process {
-        $Module = New-SqlModule init
-
         $Text -split $LinesRe | % {
             if ($_ -match $DirectiveRe) {
-                $Directive =  $Matches['dir' ]
-                $Arguments = @($Matches['args'] -split $SpacesRe | ? { $_ })
+                $Directive = $Matches['dir' ]
+                $Arguments = [string[]]@($Matches['args'] -split $SpacesRe | ? { [string] $_ })
                 switch ($Directive) {
                     'MODULE' {
-                        Out-SqlModule $Module
-                        $Module = $Arguments | Select-Object -First 1 | New-SqlModule
-                        $Module.Provides.AddRange($Arguments)
+                        $Name = $Arguments | Select-Object -First 1
+                        $Runner.StartModule($Name)
+                        $Runner.AddProvides($Arguments)
                     }
                     'PROVIDES' {
-                        $Module.Provides.AddRange($Arguments)
+                        $Runner.AddProvides($Arguments)
                     }
                     'REQUIRES' {
-                        $Module.Requires.AddRange($Arguments)
+                        $Runner.AddRequires($Arguments)
                     }
                 }
             } else {
-                $Module.Script.AppendLine($_) | Out-Null
+                $Runner.AddScriptLine($_)
             }
         }
-
-        Out-SqlModule $Module
-    }
-}
-
-function Get-Subject {
-    [OutputType([PSCustomObject])]
-    param([hashtable] $Subjects, [string] $Name)
-
-    ($Subject = $Subjects[$Name]) -or
-    ($Subject = $Subjects[$Name] = [PSCustomObject] @{
-        Name       = $Name
-        ProvidedBy = New-Object System.Collections.ArrayList
-        RequiredBy = New-Object System.Collections.ArrayList
-    }) | Out-Null
-    $Subject
-}
-
-function Merge-SqlModules {
-    param (
-        [Parameter(ValueFromPipeline)]
-        [object[]] $Modules
-    )
-    begin {
-        $Queue    = New-Object System.Collections.Queue
-        $Subjects = @{}
-    }
-    process {
-        foreach ($Module in $Modules) {
-            foreach ($Name in $Module.Provides) {
-                (Get-Subject $Subjects $Name).ProvidedBy.Add($Module) | Out-Null
-            }
-            foreach ($Name in $Module.Requires) {
-                (Get-Subject $Subjects $Name).RequiredBy.Add($Module) | Out-Null
-            }
-            if (!$Module.Requires) {
-                $Queue.Enqueue($Module)
-            }
-        }
-    }
-    end {
-        $Missing = (
-            $Subjects.Values | ? { !$_.ProvidedBy } | % { $_.Name } | sort -Unique
-        ) -join ", "
-        if ($Missing) {
-            throw "Subjects are required but not provided: $Missing"
-        }
-        Write-Output ([PSCustomObject] @{
-            Queue    = $Queue
-            Subjects = $Subjects
-        })
     }
 }
 
 function Invoke-SqlModules {
     [CmdletBinding()]
     param (
-        # The set of modules to execute.
-        [Parameter(Position = 1, Mandatory)]
-        $ModuleSet,
+        # Text containing the modules to execute.
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string] $Text,
 
         # Name of the server.  Must be a valid hostname or IP address, with an optional instance suffix (ex: "10.12.34.56\DEV").  A dot (".") may be used to specify a local server.
-        [Parameter(Position = 2, ValueFromPipelineByPropertyName)]
+        [Parameter(Position = 2)]
         [string] $Server = ".",
 
         # Name of the initial database.  If not given, the initial database is the SQL Server default database.
-        [Parameter(Position = 3, ValueFromPipelineByPropertyName)]
+        [Parameter(Position = 3)]
         [string] $Database,
 
         # Use SQL credentials instead of Windows authentication.  Must be used with -Password.
@@ -174,184 +99,42 @@ function Invoke-SqlModules {
         # Command timeout, in seconds.  0 disables timeout.  The default is 0.
         [int] $Timeout = 0
     )
+    process {
+        $Params = New-Object PSql.ModuleRunnerParameters
+        $Params.Server      = $Server
+        $Params.Database    = $Database
+        $Params.Login       = $Login
+        $Params.Password    = $Password
+        $Params.Timeout     = $Timeout
+        $Params.PSqlPath    = $PSScriptRoot
+        $Params.ScriptBlock = $ThreadMain
 
-    $OldInformationPreference = $InformationPreference
-    $InformationPreference = "Continue"
-
-    $Workers = foreach ($Id in 1..$env:NUMBER_OF_PROCESSORS) {
-        Write-Information "[Thread $Id]: Starting"
-
-        # Create worker thread to run modules asynchronously
-        $Shell = [powershell]::Create()
-        $State = $Shell.Runspace.SessionStateProxy
-        $State.SetVariable('ModuleSet', $ModuleSet   )
-        $State.SetVariable('Server'  ,  $Server      )
-        $State.SetVariable('Database',  $Database    )
-        $State.SetVariable('Login'   ,  $Login       )
-        $State.SetVariable('Password',  $Password    )
-        $State.SetVariable('Timeout' ,  $Timeout     )
-        $State.SetVariable('PSqlDir' ,  $PSScriptRoot)
-        $Shell.AddScript($ThreadMain) | Out-Null
-
-        # Pipe worker output streams to main output streams
-        foreach ($Kind in 'Debug', 'Verbose', 'Information', 'Warning' ,'Error') {
-            Register-ObjectEvent $Shell.Streams.$Kind DataAdded -SupportEvent `
-                -MessageData @{ Id = $Id; Kind = $Kind } `
-                -Action {
-                    $Id   = $Event.MessageData.Id
-                    $Kind = $Event.MessageData.Kind
-                    $Event.Sender.ReadAll() | % {
-                        if ($Kind -ne 'Error') {
-                            & "Write-$Kind" "[Thread $Id]: $_"
-                        } else {
-                            $Host.UI.WriteErrorLine("[Thread $Id]: $_")
-                        }
-                    }
-                } `
-            | Out-Null 
-        }
-
-        # Start worker
-        $Invocation = $Shell.BeginInvoke()
-        @{ Id = $Id; Shell = $Shell; Invocation = $Invocation }
+        $Runner = New-Object PSql.ModuleRunner $Params
+        Read-SqlModules $Text $Runner
+        $Runner.Complete()
+        $Runner.Run()
     }
-
-    # Wait for workers to finish
-    $Failed = $false
-    foreach ($Worker in $Workers) {
-        try {
-            if ($Failed) {
-                $Worker.Shell.Stop()
-                Write-Information "[Thread $($Worker.Id)]: Ended"
-            } else {
-                $Worker.Shell.EndInvoke($Worker.Invocation) | Out-Null
-                Write-Information "[Thread $($Worker.Id)]: Ended"
-            }
-        }
-        catch {
-            $Failed = $true
-            $Host.UI.WriteErrorLine("[Thread $($Worker.Id)]: $($_.Exception.Message)")
-            $Host.UI.WriteErrorLine("[Thread $($Worker.Id)]: Terminated due to error")
-            throw 'One or more threads ended with an error.'
-        }
-    }
-
-    # Trying to stop all threads is too difficult right now.
-    #if ($Failed) {
-    #    throw 'One or more threads ended with an error.'
-    #}
-
-    $InformationPreference = $OldInformationPreference 
 }
 
 $ThreadMain = {
-    $ErrorActionPreference = "Stop"
-    Import-Module $PSqlDir\PSql.psm1 -Force
+    #$ErrorActionPreference = "Stop"
+    Import-Module $PSqlPath\PSql.psm1 -Force
 
-    function Invoke-SqlModules {
-        param ($ModuleSet)
+    #throw 'this is an error'
 
-        $Queue      = $ModuleSet.Queue
-        $Subjects   = $ModuleSet.Subjects
-        $Module     = @{ Value = $null }
+    $Connection = Connect-Sql $Server $Database `
+        -Login $Login -Password $Password -PassThru
 
-        $Connection = Connect-Sql $Server $Database `
-            -Login $Login -Password $Password -PassThru
+    #$Connection | fl -Force | Write-Host
 
-        try {
-            while ($true) {
-                # Advance to next module
-                Use-Lock $Queue {
-                    # Mark prior module completed
-                    if ($Module["Value"]) {
-                        Complete-SqlModule $Module["Value"] $ModuleSet
-                    }
+    while ($true) {
+        $Module = $Modules.Next()
+        if (!$Module) { break }
 
-                    # Dequeue next module
-                    while ($true) {
-                        if ($Queue.Count) {
-                            # Take next queued module
-                            $Module["Value"] = $Queue.Dequeue()
-                            break
-                        } elseif (!$Subjects.Count) {
-                            # No modules queued, none in progress; done
-                            $Module["Value"] = $null
-                            break
-                        } else {
-                            # Wait for other modules to finish
-                            [System.Threading.Monitor]::Wait($Queue)
-                        }
-                    }
-                }
-
-                # Check if done
-                if (!$Module["Value"]) { return }
-
-                # Run module
-                Write-Verbose "Running $($Module["Value"].Name)"
-
-                $Module["Value"].Script `
-                    | Split-SqlBatches `
-                    | Invoke-Sql -Connection $Connection -Timeout $Timeout
-            }
-        }
-        finally {
-            Disconnect-Sql $Connection
-        }
+        $Module.Script `
+            | Split-SqlBatches `
+            | Invoke-Sql -Connection $Connection -Timeout $Timeout
     }
 
-    function Complete-SqlModule($Module, $ModuleSet) {
-        $Queue       = $ModuleSet.Queue
-        $Subjects    = $ModuleSet.Subjects
-        $WorkChanged = $false
-
-        # Update each subject provided by this module
-        foreach ($Name in $Module.Provides) {
-            $Subject = $Subjects[$Name]
-
-            # Mark this module as done
-            $Subject.ProvidedBy.Remove($Module)
-
-            # Check if all subject's modules are done
-            if ($Subject.ProvidedBy) { continue }
-
-            # Mark subject as done
-            $Subjects.Remove($Name)
-            if (!$Subjects.Count) { $WorkChanged = $true }
-
-            # Update dependents
-            foreach ($Dependent in $Subject.RequiredBy) {
-
-                # Mark requirement as met
-                $Dependent.Requires.Remove($Name)
-
-                # Check if all of dependent's requrements are met
-                if ($Dependent.Requires) { continue }
-
-                # All requirements met; queue the dependent
-                $Queue.Enqueue($Dependent)
-                $WorkChanged = $true
-            }
-        }
-
-        if ($WorkChanged) {
-            [System.Threading.Monitor]::PulseAll($Queue)
-        }
-    }
-
-    function Use-Lock {
-        param([object] $Object, [scriptblock] $ScriptBlock)
-        $Locked = $false
-        try {
-            [System.Threading.Monitor]::Enter($Object, [ref] $Locked)
-            & $ScriptBlock
-        }
-        finally {
-            if ($Locked) {
-                [System.Threading.Monitor]::Exit($Object)
-            }
-        }
-    }
-
-    Invoke-SqlModules $ModuleSet
+    Disconnect-Sql $Connection
 }
---
+++ PSql.ModuleRunner.cs
@@ -0,0 +1,322 @@
+namespace PSql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ModuleRunnerParameters
+    {
+        public string Server   { get; set; }
+        public string Database { get; set; }
+        public string Login    { get; set; }
+        public string Password { get; set; }
+        public int    Timeout  { get; set; }
+        public string PSqlPath { get; set; }
+
+        public string ScriptBlock { get; set; }
+    }
+
+    public class Module
+    {
+        public Module(string name)
+        {
+            Name     = Helpers.RequireName(name);
+            Provides = new SortedSet<string>();
+            Requires = new SortedSet<string>();
+            Script   = new StringBuilder();    
+            Provides.Add(name);
+        }
+
+        public string            Name     { get; private set; }
+        public SortedSet<string> Provides { get; private set; }
+        public SortedSet<string> Requires { get; private set; }
+        public StringBuilder     Script   { get; private set; }
+    }
+
+    public class Subject
+    {
+        public Subject(string name)
+        {
+            Name = Helpers.RequireName(name);
+            ProvidedBy = new List<Module>();
+            RequiredBy = new List<Module>();
+        }
+
+        public string       Name       { get; private set; }
+        public List<Module> ProvidedBy { get; private set; }
+        public List<Module> RequiredBy { get; private set; }
+    }
+
+    public class ModuleRunner
+    {
+        private static readonly Regex EolRegex = new Regex(
+            @"\r?\n",
+            RegexOptions.Compiled |
+            RegexOptions.CultureInvariant
+        );
+
+        private static readonly Regex SpaceRegex = new Regex(
+            @"\s+",
+            RegexOptions.Compiled |
+            RegexOptions.CultureInvariant
+        );
+
+        private static readonly Regex DirectiveRegex = new Regex(
+            @"^ --\# \s+ (?<dir>MODULE|PROVIDES|REQUIRES): \s+ (?<args>.*) $",
+            RegexOptions.Compiled |
+            RegexOptions.CultureInvariant |
+            RegexOptions.IgnorePatternWhitespace
+        );
+
+        private readonly object
+            _lock = new object();
+
+        private readonly Queue<Module>
+            _queue = new Queue<Module>();
+
+        private readonly Dictionary<string, Subject>
+            _subjects = new Dictionary<string, Subject>();
+
+        private readonly ModuleRunnerParameters
+            _parameters;
+
+        private Module _module;
+
+        public ModuleRunner(ModuleRunnerParameters parameters)
+        {
+            if (parameters == null)
+                throw new ArgumentNullException("parameters");
+
+            _parameters = parameters;
+            _module = new Module("init");
+        }
+
+        public void StartModule(string name)
+        {
+            AcceptModule();
+            _module = new Module(name);
+        }
+
+        public void AddProvides(IEnumerable<string> names)
+        {
+            RequireModule().Provides.UnionWith(names);
+        }
+
+        public void AddRequires(IEnumerable<string> names)
+        {
+            RequireModule().Requires.UnionWith(names);
+        }
+
+        public void AddScriptLine(string text)
+        {
+            RequireModule().Script.AppendLine(text);
+        }
+
+        private void AcceptModule()
+        {
+            var module = RequireModule();
+
+            foreach (var name in module.Provides)
+                GetOrAddSubject(name).ProvidedBy.Add(module);
+
+            foreach (var name in module.Requires)
+                GetOrAddSubject(name).RequiredBy.Add(module);
+
+            if (!module.Requires.Any())
+                _queue.Enqueue(module);
+
+            _module = null;
+        }
+
+        private Module RequireModule()
+        {
+            var module = _module;
+            if (module == null)
+                throw new InvalidOperationException(
+                    "Module definitions are frozen after Complete() is called."
+                );
+            return module;
+        }
+
+        private Subject GetOrAddSubject(string name)
+        {
+            Helpers.RequireName(name);
+            Subject subject;
+
+            if (!_subjects.TryGetValue(name, out subject))
+                _subjects[name] = subject = new Subject(name);
+
+            return subject;
+        }
+
+        public void Complete()
+        {
+            AcceptModule();
+
+            var missing = string.Join
+            (
+                ", ",
+                _subjects.Values
+                    .Where(s => !s.ProvidedBy.Any())
+                    .Select(s => s.Name)
+                    .OrderBy(n => n)
+            );
+
+            if (missing.Any())
+                throw new InvalidOperationException(
+                    "Modules are required but not provided: " + missing
+                );
+        }
+
+        public void Run()
+        {
+            Parallel.For(1, Environment.ProcessorCount + 1, ThreadMain);
+        }
+
+        // Thread-safe
+        private Module GetNextModule(Module priorModule)
+        {
+            lock (_lock)
+            {
+                if (priorModule != null)
+                    CompleteModule(priorModule);
+
+                return DequeueModule();
+            }
+        }
+
+        private Module DequeueModule()
+        {
+            for (;;)
+            {
+                if (_queue.Any())
+                    return _queue.Dequeue();
+                else if (!_subjects.Any())
+                    return null; // nothing left to do
+                else
+                    Monitor.Wait(_lock, 1000);
+            }
+        }
+
+        private void CompleteModule(Module module)
+        {
+            var notify = false; // Notify waiting workers to get the next module.
+
+            foreach (var name in module.Provides)
+            {
+                var subject = _subjects[name];
+
+                // Mark this module as done
+                subject.ProvidedBy.Remove(module);
+
+                // Check if all subject's modules are done
+                if (subject.ProvidedBy.Any())
+                    continue;
+
+                // Mark subject as done
+                _subjects.Remove(name);
+                if (!_subjects.Any())
+                    // No more modules; wake sleeping workers so they can exit
+                    notify = true;
+
+                // Update dependents
+                foreach (var dependent in subject.RequiredBy)
+                {
+                    // Mark requirement as met
+                    dependent.Requires.Remove(name);
+
+                    // Check if all of dependent's requrements are met
+                    if (dependent.Requires.Any())
+                        continue;
+
+                    // All requirements met; queue the dependent
+                    _queue.Enqueue(dependent);
+                    notify = true;
+                }
+            }
+
+            // Wake up sleeping workers
+            if (notify)
+                Monitor.PulseAll(_lock);
+        }
+
+        public class ModuleDispenser
+        {
+            private readonly ModuleRunner _runner;
+            private          Module       _module;
+
+            internal ModuleDispenser(ModuleRunner runner)
+            {
+                _runner = runner;
+            }
+
+            // Thread-safe
+            public Module Next()
+            {
+                return _module = _runner.GetNextModule(_module);
+            }
+        }
+
+        private static void Echo(int id, string text)
+        {
+            Console.WriteLine("[Worker {0}]: {1}", id, text);
+        }
+
+        private void ThreadMain(int id)
+        {
+            Echo(id, "Starting");
+            var shell = PowerShell.Create();
+
+            var state = shell.Runspace.SessionStateProxy;
+            state.SetVariable("Modules" ,  new ModuleDispenser(this));
+            state.SetVariable("Server"  ,  _parameters.Server  );
+            state.SetVariable("Database",  _parameters.Database);
+            state.SetVariable("Login"   ,  _parameters.Login   );
+            state.SetVariable("Password",  _parameters.Password);
+            state.SetVariable("Timeout" ,  _parameters.Timeout );
+            state.SetVariable("PSqlPath",  _parameters.PSqlPath );
+
+            shell.AddScript(_parameters.ScriptBlock);
+
+            var streams = shell.Streams;
+            streams.Debug       .DataAdded += HandleData<DebugRecord      >(id);
+            streams.Verbose     .DataAdded += HandleData<VerboseRecord    >(id);
+            streams.Information .DataAdded += HandleData<InformationRecord>(id);
+            streams.Warning     .DataAdded += HandleData<WarningRecord    >(id);
+            streams.Error       .DataAdded += HandleData<ErrorRecord      >(id);
+
+            shell.Invoke();
+            Echo(id, "Ending");
+        }
+
+        private EventHandler<DataAddedEventArgs> HandleData<TRecord>(int id)
+        {
+            return new EventHandler<DataAddedEventArgs>
+            (
+                (sender, args) =>
+                {
+                    var records = (PSDataCollection<TRecord>) sender;
+                    foreach (var record in records.ReadAll())
+                        Echo(id, record.ToString());
+                }
+            );
+        }
+    }
+
+    internal class Helpers
+    {
+        internal static string RequireName(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+            if (name.Length == 0)
+                throw new ArgumentOutOfRangeException("name");
+            return name;
+        }
+    }
+}
