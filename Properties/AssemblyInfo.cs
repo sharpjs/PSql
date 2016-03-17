5bff84cfe525a8d4be3d0b58bc1ca888a5c28d89
Jeff Sharp <jeff.sharp@git>
2016-03-17 16:48:55 -0500

    Create VS project for IntelliSense and ease of editing.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PSql")]
+[assembly: AssemblyProduct("PSql")]
+[assembly: AssemblyCompany("Jeffrey Sharp")]
+[assembly: AssemblyCopyright("Copyright © 2016 Jeffrey Sharp")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
