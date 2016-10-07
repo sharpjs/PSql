7b13d70fa72de1c8e8e8ba206581b399e9e86457
Jeff Sharp <jeff.sharp@git>
2016-10-07 08:44:44 -0500

    Improve Invoke-Sql tests.

--- Test-PSql.ps1
+++ Test-PSql.ps1
@@ -62,8 +62,59 @@ test "Import-Module" {
 test "Connect-Sql" {
     $C = Connect-Sql -Server . -Database master
     assert { $C -ne $null }
-    assert {$C.GetType() -eq [System.Data.SqlClient.SqlConnection] }
+    assert { $C -is [System.Data.SqlClient.SqlConnection] }
+    assert { $C.State -eq [System.Data.ConnectionState]::Open }
+    assert { $C.DataSource -eq "." }
+    assert { $C.Database -eq "master" }
     Disconnect-Sql $C
+}
+
+test "Invoke-Sql -> nchar" {
+    $X = Invoke-Sql "select a = N'Hello';"
+    assert { $X.a -eq "Hello" }
+}
+
+test "Invoke-Sql -> int" {
+    $X = Invoke-Sql "select a = 42;"
+    assert { $X -ne $null }
+    assert { $X -is [PSCustomObject] }
+    assert { $X.a -eq 42 }
+}
+
+test "Invoke-Sql -> bit" {
+    $X = Invoke-Sql "select a = CONVERT(bit, 0), b = CONVERT(bit, 1);"
+    assert { $X.a -eq $false }
+    assert { $X.b -eq $true }
+}
+
+test "Invoke-Sql -> date" {
+    $X = Invoke-Sql "select a = CONVERT(date, SYSUTCDATETIME());"
+    assert { $X.a -eq [datetime]::UtcNow.Date }
+}
+
+test "Invoke-Sql -> time" {
+    $X = Invoke-Sql "select a = CONVERT(time, SYSUTCDATETIME());"
+    assert { [datetime]::UtcNow.TimeOfDay - $X.a -lt [timespan]::FromMinutes(1) }
+}
+
+test "Invoke-Sql -> datetime2" {
+    $X = Invoke-Sql "select a = SYSUTCDATETIME();"
+    assert { [datetime]::UtcNow - $X.a -lt [timespan]::FromMinutes(1) }
+}
+
+test "Invoke-Sql -> uniqueidentifier" {
+    $X = Invoke-Sql "select a = NEWID();"
+    assert { $X.a -is [guid] -and $X -ne [guid]::Empty }
+}
+
+test "Invoke-Sql -> binary" {
+    $X = Invoke-Sql "select a = 0xDEADBEEF;"
+    assert { $X.a -is [byte[]] }
+    assert { $X.a.Length -eq 4 }
+    assert { $X.a[0] -eq 0xDE  }
+    assert { $X.a[1] -eq 0xAD  }
+    assert { $X.a[2] -eq 0xBE  }
+    assert { $X.a[3] -eq 0xEF  }
 }
 
 Write-Host
