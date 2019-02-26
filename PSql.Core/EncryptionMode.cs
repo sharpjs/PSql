8eb3dad9bdfb66c7c5ea9d98a746668800859c68
Jeff Sharp <jeff.sharp@git>
2019-02-26 15:17:34 -0800

    Prep for specifying encryption as a mode parameter.

---
+++ PSql.Core/EncryptionMode.cs
@@ -0,0 +1,36 @@
+﻿namespace PSql
+{
+    /// <summary>
+    ///   Modes for connection encryption and server identity verification.
+    /// </summary>
+    public enum EncryptionMode
+    {
+        /// <summary>
+        ///   The default encrption mode.  Equivalent to <see cref="None"/> for
+        ///   connections to the local machine, and <see cref="Full"/> for all
+        ///   other connections.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        ///   No connection encryption or server identity check.
+        ///   Data sent over the connection is exposed to other network devices.
+        ///   A malicious device could masquerade as a server.
+        ///   This encryption mode is appropriate for same-machine connections only.
+        /// </summary>
+        None,
+
+        /// <summary>
+        ///   Connections are encrypted, but server identities are not verified.
+        ///   A malicious device could masquerade as a server.
+        ///   This encryption mode is appropriate only when the server uses a self-signed certificate.
+        /// </summary>
+        Unverified,
+
+        /// <summary>
+        ///   Connections are encrypted, and server identities are verified.
+        ///   This is the most secure encryption mode.
+        /// </summary>
+        Full
+    }
+}
