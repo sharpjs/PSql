64de3299c6aa7a92e23936f3441944b98154b3bf
Jeff Sharp <jeff.sharp@git>
2019-03-01 06:05:55 -0800

    Safer way to convert TimeSpan to int.

---
+++ PSql.Core/TimeSpanExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PSql
+{
+    internal static class TimeSpanExtensions
+    {
+        public static int GetTotalSecondsSaturatingInt32(this TimeSpan span)
+        {
+            long seconds = span.Ticks / TimeSpan.TicksPerSecond;
+
+            return seconds > int.MaxValue ? int.MaxValue
+                :  seconds < int.MinValue ? int.MinValue
+                :  (int) seconds;
+        }
+    }
+}
