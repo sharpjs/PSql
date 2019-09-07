71abcb78c6cb18189d08f69137f5c49dcf1b089f
Jeff Sharp <public@sharpjs.net>
2019-09-07 09:11:48 -0700

    Some test coverage for SqlCmdPreprocessor.

--- PSql.Core.Tests/SqlCmdPreprocessorTests.cs
+++ PSql.Core.Tests/SqlCmdPreprocessorTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -39,17 +40,181 @@ namespace PSql
         }
 
         [Test]
-        public void Process_SingleBatch()
+        public void Process_TokenlessText()
         {
             var preprocessor = new SqlCmdPreprocessor();
 
-            var batches = preprocessor.Process(
-                @"this is a single batch"
-            );
+            var text = "no tokens here";
 
-            batches.Should().Equal(
-                @"this is a single batch"
-            );
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_LineComment(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a", "b -- foo", "c");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BlockComment(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a /* foo", "bar */ b");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BlockComment_Unterminated(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a /* foo");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_SingleQuotedString(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a 'foo''bar", "baz' b");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_SingleQuotedString_Unterminated(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a 'foo''bar");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BracketQuotedIdentifier(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a [foo]]bar", "baz] b");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BracketQuotedIdentifier_Unterminated(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            var text = Lines(eol, eof, "a [foo]]bar");
+
+            var batches = preprocessor.Process(text);
+
+            batches.Should().Equal(text);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BatchSeparator(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            foreach (var separator in BatchSeparators)
+            {
+                var text = Lines(eol, eof, "a", separator, "b");
+
+                var batches = preprocessor.Process(text);
+
+                batches.Should().Equal("a" + eol, "b" + eof);
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BatchSeparator_Repeated(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            foreach (var separator in BatchSeparators)
+            {
+                var text = Lines(eol, eof, "a", separator, separator, "b");
+
+                var batches = preprocessor.Process(text);
+
+                batches.Should().Equal("a" + eol, "b" + eof);
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(EolEofCases))]
+        public void Process_BatchSeparator_AtEof(string eol, string eof)
+        {
+            var preprocessor = new SqlCmdPreprocessor();
+
+            foreach (var separator in BatchSeparators)
+            {
+                var text = Lines(eol, eof, "a", separator);
+
+                var batches = preprocessor.Process(text);
+
+                batches.Should().Equal("a" + eol);
+            }
+        }
+
+        private static readonly string[][] EolEofCases =
+        {
+            //         EOL     EOF
+            new[] {   "\n",     "" },
+            new[] { "\r\n",     "" },
+            new[] {   "\n",   "\n" },
+            new[] { "\r\n", "\r\n" }
+        };
+
+        private static readonly string[] BatchSeparators =
+        {
+            "GO", "Go", "gO", "go"
+        };
+
+        private static string Lines(string eol, string eof, params string[] lines)
+        {
+            var text = new StringBuilder(capacity: 64);
+
+            if (lines.Length > 0)
+                text.Append(lines[0]);
+
+            for (var i = 1; i < lines.Length; i++)
+                text.Append(eol).Append(lines[i]);
+
+            return text.Append(eof).ToString();
         }
     }
 }
