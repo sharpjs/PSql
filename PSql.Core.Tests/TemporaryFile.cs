704ddb22effea5c55324966e11dd3892fe73dbe6
Jeff Sharp <public@sharpjs.net>
2019-09-08 10:58:01 -0700

    Temporary file in IDisposable wrapper.

---
+++ PSql.Core.Tests/TemporaryFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using IO = System.IO;
+
+namespace PSql
+{
+    internal class TemporaryFile : IDisposable
+    {
+        public TemporaryFile()
+        {
+            Path = IO.Path.GetTempFileName();
+        }
+
+        public string Path { get; }
+
+        ~TemporaryFile()
+        {
+            Dispose(managed: false);
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose(managed: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool managed)
+        {
+            IO.File.Delete(Path);
+        }
+    }
+}
