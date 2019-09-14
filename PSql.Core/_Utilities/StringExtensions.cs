18bd20600fc64b71010e7dbaa51b2e2679e73e71
Jeff Sharp <public@sharpjs.net>
2019-09-14 13:35:31 -0700

    Add string.NullIfEmpty()

---
+++ PSql.Core/_Utilities/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace PSql
+{
+    internal static class StringExtensions
+    {
+        internal static string NullIfEmpty(this string s)
+            => string.IsNullOrEmpty(s) ? null : s;
+    }
+}
