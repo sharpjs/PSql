d86be1965cf67aca84f23c21b01523a3e5d6dd2c
Jeff Sharp <public@sharpjs.net>
2019-09-14 10:26:09 -0700

    Simplify parameters.

--- PSql.Core/_Commands/NewSqlContextCommand.cs
+++ PSql.Core/_Commands/NewSqlContextCommand.cs
@@ -13,61 +13,59 @@ namespace PSql
             AzureName   = "Azure";
 
         // -Azure
-        [Alias("a")]
         [Parameter(ParameterSetName = AzureName, Position = 0, Mandatory = true, ValueFromPipelineByPropertyName = true)]
         public SwitchParameter Azure { get; set; }
 
         // -ResourceGroupName
-        [Alias("g", "rg", "ResourceGroup")]
+        [Alias("ResourceGroup")]
         [Parameter(ParameterSetName = AzureName, Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty]
         public string ResourceGroupName { get; set; }
 
         // -ServerName
-        [Alias("s", "sn", "Server")]
+        [Alias("Server")]
         [Parameter(ParameterSetName = GenericName, Position = 0,                   ValueFromPipelineByPropertyName = true)]
         [Parameter(ParameterSetName = AzureName,   Position = 2, Mandatory = true, ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty]
         public string ServerName { get; set; }
 
         // -DatabaseName
-        [Alias("d", "dn", "Database")]
+        [Alias("Database")]
         [Parameter(ParameterSetName = GenericName, Position = 1, ValueFromPipelineByPropertyName = true)]
         [Parameter(ParameterSetName = AzureName,   Position = 3, ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty]
         public string DatabaseName { get; set; }
 
         // -Credential
-        [Alias("c")]
         [Parameter(ParameterSetName = GenericName, Position = 2,                   ValueFromPipelineByPropertyName = true)]
         [Parameter(ParameterSetName = AzureName,   Position = 4, Mandatory = true, ValueFromPipelineByPropertyName = true)]
         [Credential]
         public PSCredential Credential { get; set; } = PSCredential.Empty;
 
         // -EncryptionMode
-        [Alias("e", "em", "Encryption")]
+        [Alias("Encryption")]
         [Parameter(ParameterSetName = GenericName, ValueFromPipelineByPropertyName = true)]
         public EncryptionMode EncryptionMode { get; set; }
 
         // -ReadOnlyIntent
-        [Alias("ro", "ReadOnly")]
+        [Alias("ReadOnly")]
         [Parameter(ValueFromPipelineByPropertyName = true)]
         public SwitchParameter ReadOnlyIntent { get; set; }
 
         // -ClientName
-        [Alias("cn", "Client")]
+        [Alias("Client")]
         [Parameter(ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty]
         public string ClientName { get; set; }
 
         // -ApplicationName
-        [Alias("an", "Application")]
+        [Alias("Application")]
         [Parameter(ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty]
         public string ApplicationName { get; set; }
 
         // -ConnectTimeout
-        [Alias("t", "to", "Timeout")]
+        [Alias("Timeout")]
         [Parameter(ValueFromPipelineByPropertyName = true)]
         [ValidateRange("0:00:00", "24855.03:14:07")]
         public TimeSpan? ConnectTimeout { get; set; }
