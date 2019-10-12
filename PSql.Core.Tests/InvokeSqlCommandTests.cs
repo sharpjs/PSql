220cda09cb21fe00e04771797fccf57d9a3d85b8
Jeff Sharp <public@sharpjs.net>
2019-10-12 10:29:24 -0700

    A very simple test using the module.

---
+++ PSql.Core.Tests/InvokeSqlCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using static PSql.ScriptExecutor;
+
+namespace PSql
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class InvokeSqlCommandTests
+    {
+        [Test]
+        public void JustATest()
+        {
+            Execute(@"
+                Invoke-Sql ""PRINT 42;""
+            ");
+        }
+    }
+}
