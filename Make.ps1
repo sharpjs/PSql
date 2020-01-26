0fb278d2d3c71542efdb2177eb32eac0e46b16e8
Jeff Sharp <public@sharpjs.net>
2020-01-26 10:48:00 -0800

    Switch to Coverlet+ReportGenerator for code coverage.

--- Make.ps1
+++ Make.ps1
@@ -33,6 +33,10 @@ param (
     [Parameter(Mandatory, ParameterSetName="Coverage")]
     [switch] $Coverage,
 
+    # Open code coverage report in a web browser.
+    [Parameter(ParameterSetName="Coverage")]
+    [switch] $Open,
+
     # The configuration to build: Debug or Release.  The default is Debug.
     [Parameter(ParameterSetName="Build")]
     [Parameter(ParameterSetName="Test")]
@@ -47,8 +51,6 @@ Set-StrictMode -Version Latest
 
 $Command = $PSCmdlet.ParameterSetName
 if ($Command -eq "Test") { $Test = $true }
-
-$AssemblyNameRoot = "PSql.Core"
 
 # http://patorjk.com/software/taag/#p=display&f=Slant
 Write-Host -ForegroundColor Cyan @' 
@@ -65,37 +67,47 @@ function Main {
     Invoke-Build
 
     if ($Test -or $Coverage) {
-        Set-Location -LiteralPath "$AssemblyNameRoot.Tests"
-        #Invoke-TestForTargetFramework net472
-        Invoke-TestForTargetFramework netcoreapp2.1
+        Invoke-Test
+    }
+
+    if ($Coverage) {
+        Export-CoverageReport
     }
 } 
 
 function Invoke-Build {
     Write-Phase "Build"
-    Invoke-DotNetExe build --configuration $Configuration
+    Invoke-DotNetExe build --configuration:$Configuration
 }
 
-function Invoke-TestForTargetFramework {
-    param (
-        [Parameter(Mandatory)]
-        [string] $TargetFramework
+function Invoke-Test {
+    Write-Phase "Test$(if ($Coverage) {" + Coverage"})"
+    Remove-Item test -Recurse -ErrorAction SilentlyContinue
+    Invoke-DotNetExe -Arguments @(
+        "test"
+        "--no-build"
+        "--configuration:$Configuration"
+        if ($Coverage) {
+            "--collect:XPlat Code Coverage"
+            "--results-directory:test"
+        }
+    )
+}
+
+function Export-CoverageReport {
+    Write-Phase "Coverage Report"
+    Invoke-DotNetExe -Arguments "tool", "restore"
+    Invoke-DotNetExe -Arguments @(
+        "reportgenerator"
+        "-reports:test\*\coverage.cobertura.xml"
+        "-targetdir:coverage"
+        "-reporttypes:HtmlInline_AzurePipelines_Dark;TeamCitySummary;Badges"
+        "-verbosity:Warning"
     )
 
-    Write-Phase "Test: $TargetFramework$(if ($Coverage) {" + Coverage"})"
-    Invoke-DotNetExe -Arguments @(
-        if ($Coverage) {
-            "dotcover"
-                "--dcReportType=HTML"
-                "--dcOutput=..\coverage\$TargetFramework.html"
-                "--dcFilters=+:$AssemblyNameRoot`;+:$AssemblyNameRoot.*`;-:*.Tests"
-                "--dcAttributeFilters=System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute"
-        }
-        "test"
-            "--framework:$TargetFramework"
-            "--configuration:$Configuration"
-            "--no-build"
-    )
+    if ($Open) {
+        & .\coverage\index.htm
+    }
 }
 
 function Invoke-DotNetExe {
