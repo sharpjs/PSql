911c4cc2e347a5efe1541227dde2c5ad266dc57b
Jeff Sharp <public@sharpjs.net>
2020-11-14 09:52:13 -0800

    Move test-support files to their own folder.

--- PSql.Tests/Support/ScriptExecutor.cs
+++ PSql.Tests/Support/ScriptExecutor.cs
@@ -19,10 +19,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
-using System.Text;
 using Microsoft.PowerShell;
 using NUnit.Framework;
-using static System.Char;
 
 #nullable enable
 
@@ -85,57 +83,5 @@ namespace PSql
         internal static string EscapeForDoubleQuoteString(this string s)
             => s.Replace("\"", "`\"")
                 .Replace("`",  "``");
-
-        internal static string Unindent(this string s)
-        {
-            if (s is null)
-                throw new ArgumentNullException(nameof(s));
-
-            // Take shortcut for empty strings
-            if (s.Length == 0)
-                return s;
-
-            // Compute minimum indent of all lines
-            var start  = 0;
-            var indent = int.MaxValue;
-            var count  = 0;
-            do
-            {
-                var index = s.IndexOfNonWhitespace(start);
-                indent    = Math.Min(indent, index - start);
-                start     = s.IndexOfNextLine(index);
-                count++;
-            }
-            while (start < s.Length);
-
-            // Take shortcut for non-indented strings
-            if (indent == 0)
-                return s;
-
-            // Build unindented string
-            var result = new StringBuilder(s.Length - indent * count);
-            start = indent;
-            do
-            {
-                var index = s.IndexOfNextLine(start);
-                result.Append(s, start, index - start);
-                start = index + indent;
-            }
-            while (start < s.Length);
-
-            return result.ToString();
-        }
-
-        private static int IndexOfNonWhitespace(this string s, int index)
-        {
-            while (index < s.Length && IsWhiteSpace(s, index)) { index++; }
-            return index;
-        }
-
-        private static int IndexOfNextLine(this string s, int index)
-        {
-            while (index < s.Length && s[index++] != '\n') { }
-            return index;
-        }
     }
 }
