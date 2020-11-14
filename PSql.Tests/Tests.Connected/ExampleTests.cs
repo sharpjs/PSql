4756d24a6a9e3fd153a4b731eae801cac9956625
Jeff Sharp <public@sharpjs.net>
2020-11-14 07:19:42 -0800

    Execute test command against containerized server.

--- PSql.Tests/Tests.Connected/ExampleTests.cs
+++ PSql.Tests/Tests.Connected/ExampleTests.cs
@@ -1,13 +1,38 @@
+using System;
 using NUnit.Framework;
 
 namespace PSql.Tests.Connected
 {
+    using static FormattableString;
+
     [TestFixture]
     public class ExampleTests
     {
         [Test]
         public void Foo()
         {
+            Execute(@"
+                Invoke-Sql -Context $Context ""
+                    SELECT X=1;
+                ""
+            ");
+        }
+
+        private static SqlServer Server => ConnectedTestsSetup.SqlServer;
+
+        private readonly string Prelude = Invariant($@"
+            $Credential = [PSCredential]::new(
+                ""{Server.Credential.UserName}"",
+                ""{Server.Credential.Password}""
+            )
+            $Context = New-SqlContext `
+                -ServerName .,{Server.Port} `
+                -Credential $Credential
+        ").Unindent();
+
+        private void Execute(string script)
+        {
+            ScriptExecutor.Execute(Prelude + script.Unindent());
         }
     }
 }
