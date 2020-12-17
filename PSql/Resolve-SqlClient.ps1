f8203c6923ccd8778e09151cc3faa529dc3fb1a0
Jeff Sharp <public@sharpjs.net>
2020-12-17 08:58:32 -0800

    Don't attempt to load assemblies that are loaded already.

--- PSql/Resolve-SqlClient.ps1
+++ PSql/Resolve-SqlClient.ps1
@@ -1,11 +1,13 @@
-if ($IsWindows)
-{
-    Add-Type -Path (Join-Path $PSScriptRoot runtimes\win\lib\netcoreapp3.1\Microsoft.Data.SqlClient.dll)
-}
-else
-{
-    Add-Type -Path (Join-Path $PSScriptRoot runtimes/unix/lib/netcoreapp3.1/Microsoft.Data.SqlClient.dll)
-}
+@(
+    # Required for basic usage
+    if (-not ('Microsoft.Data.SqlClient.SqlConnection' -as [type])) {
+        Join-Path $PSScriptRoot runtimes ($IsWindows ? "win" : "unix") `
+            lib netcoreapp3.1 Microsoft.Data.SqlClient.dll
+    }
 
-# Required for Azure Active Directory authentication modes
-Add-Type -Path (Join-Path $PSScriptRoot Microsoft.Identity.Client.dll)
+    # Required for Azure Active Directory authentication modes
+    if (-not ('Microsoft.Identity.Client.IAccount' -as [type])) {
+        Join-Path $PSScriptRoot Microsoft.Identity.Client.dll
+    }
+) `
+| ForEach-Object { Add-Type -Path $_ }
