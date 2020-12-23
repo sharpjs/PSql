7f89a454cf6b66026ad6c37bf0ea0a6b1c8da307
Jeff Sharp <public@sharpjs.net>
2020-12-23 14:38:09 -0800

    Documentation.

--- PSql/_Commands/Cmdlet.cs
+++ PSql/_Commands/Cmdlet.cs
@@ -37,7 +37,7 @@ namespace PSql
             // works by sending specially-marked messages to the information
             // stream.
             //
-            // https://github.com/PowerShell/PowerShell/blob/v6.1.3/src/Microsoft.PowerShell.Commands.Utility/commands/utility/WriteConsoleCmdlet.cs
+            // https://github.com/PowerShell/PowerShell/blob/v7.0.3/src/Microsoft.PowerShell.Commands.Utility/commands/utility/WriteConsoleCmdlet.cs
 
             var data = new HostInformationMessage
             {
@@ -61,7 +61,36 @@ namespace PSql
             WriteInformation(data, HostTag);
         }
 
-        private protected (SqlConnection, bool owned) EnsureConnection(
+        /// <summary>
+        ///   Returns the specified shared <see cref="SqlConnection"/> instance
+        ///   if provided, or creates a new, owned instance using the specified
+        ///   context and database name.
+        /// </summary>
+        /// <param name="connection">
+        ///   The shared connection.  If provided, the method returns this
+        ///   connection.
+        /// </param>
+        /// <param name="context">
+        ///   An object containing information necessary to connect to a SQL
+        ///   Server or compatible database if <paramref name="connection"/> is
+        ///   <c>null</c>.  If not provided, the method will use a context with
+        ///   default property values as necessary.
+        /// </param>
+        /// <param name="databaseName">
+        ///   The name of the database to which to connect if
+        ///   <paramref name="connection"/> is <c>null</c>.  If not provided,
+        ///   the method connects to the default database for the context.
+        /// </param>
+        /// <returns>
+        ///   A tuple consisting of the resulting connection and a value that
+        ///   indicates whether the caller owns the connection and must ensure
+        ///   its disposal.  If <paramref name="connection"/> is provided, the
+        ///   method returns that connection and <c>false</c> (shared).
+        ///   Otherwise, the method creates a new connection as specified by
+        ///   <paramref name="context"/> and <paramref name="databaseName"/>
+        ///   and returns the connection and <c>true</c> (owned).
+        /// </returns>
+        protected (SqlConnection, bool owned) EnsureConnection(
             SqlConnection? connection,
             SqlContext?    context,
             string?        databaseName)
