38e9fa2d6c773f2b51ab406a49796686e0721f35
Jeff Sharp <public@sharpjs.net>
2023-06-07 16:14:15 -0700

    Document.

--- PSql/_Commands/Cmdlet.cs
+++ PSql/_Commands/Cmdlet.cs
@@ -1,20 +1,6 @@
-/*
-    Copyright 2021 Jeffrey Sharp
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
 
-    Permission to use, copy, modify, and distribute this software for any
-    purpose with or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
-
-using System.Management.Automation;
 using System.Management.Automation.Host;
 
 namespace PSql;
@@ -27,20 +13,38 @@ public abstract class Cmdlet : System.Management.Automation.Cmdlet
     private static readonly string[]
         HostTag = { "PSHOST" };
 
-    public void WriteHost(string message,
+    /// <summary>
+    ///   Writes the specified message to the host.
+    /// </summary>
+    /// <param name="message">
+    ///   The message to write.
+    /// </param>
+    /// <param name="newLine">
+    ///   Whether a newline should follow the message.
+    /// </param>
+    /// <param name="foregroundColor">
+    ///   The foreground color to use.
+    /// </param>
+    /// <param name="backgroundColor">
+    ///   The background color to use.
+    /// </param>
+    /// <remarks>
+    ///   This method is similar to the PowerShell <c>Write-Host</c> cmdlet.
+    /// </remarks>
+    public void WriteHost(
+        string        message,
         bool          newLine         = true,
         ConsoleColor? foregroundColor = null,
         ConsoleColor? backgroundColor = null)
     {
-        // Technique learned from PSv5+ Write-Host implementation, which
-        // works by sending specially-marked messages to the information
-        // stream.
+        // Technique learned from PSv5+ Write-Host implementation, which works
+        // by sending specially-marked messages to the information stream.
         //
         // https://github.com/PowerShell/PowerShell/blob/v7.0.3/src/Microsoft.PowerShell.Commands.Utility/commands/utility/WriteConsoleCmdlet.cs
 
         var data = new HostInformationMessage
         {
-            Message   = message,
+            Message   = message ?? "",
             NoNewLine = !newLine
         };
 
