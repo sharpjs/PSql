ed8e866671023124456b594f811360b04d45d770
Jeff Sharp <public@sharpjs.net>
2023-06-07 16:33:15 -0700

    Change copyright to my company. Usings.

--- PSql.Tests/Tests.Unit/AzureSqlContextTests.cs
+++ PSql.Tests/Tests.Unit/AzureSqlContextTests.cs
@@ -1,26 +1,11 @@
-/*
-    Copyright 2021 Jeffrey Sharp
-
-    Permission to use, copy, modify, and distribute this software for any
-    purpose with or without fee is hereby granted, provided that the above
-    copyright notice and this permission notice appear in all copies.
-
-    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
+// Copyright 2023 Subatomix Research Inc.
+// SPDX-License-Identifier: ISC
 
 using System.Linq.Expressions;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Reflection;
-using FluentAssertions;
 using FluentAssertions.Extensions;
-using NUnit.Framework;
 
 namespace PSql.Tests.Unit;
 
